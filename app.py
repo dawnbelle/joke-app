a21db2cbbc8e51ef7212d2e7f1e8cdf2833e1b05
Ray Belle <bellexdawn@gmail.com>
2022-07-13 15:32:08 -0700

    Added data layer for refactoring.

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 import flask
+from data import jokes
 from flask import Flask
 import random
 
 app = Flask(__name__)
-
-jokes = [
-    "What can you do if you cannot push your git changes? Use the --force, Luke",
-    "Why was the function sad after a successful first call? It didn’t get a callback.",
-    "What did the spider do on the computer? Made a website!",
-    "What did the computer do at lunchtime? Had a byte!",
-]
 
 @app.get("/")
 def tell_a_joke():
---
+++ data.py
@@ -0,0 +1,6 @@
+jokes = [
+    "What can you do if you cannot push your git changes? Use the --force, Luke",
+    "Why was the function sad after a successful first call? It didn’t get a callback.",
+    "What did the spider do on the computer? Made a website!",
+    "What did the computer do at lunchtime? Had a byte!",
+]
