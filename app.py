7b85798125e2bacee55be8e1f7987741f52c1276
Ray Belle <bellexdawn@gmail.com>
2022-07-13 15:19:19 -0700

    Added html template for joke view.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask
 import random
 
@@ -5,15 +6,12 @@ app = Flask(__name__)
 
 jokes = [
     "What can you do if you cannot push your git changes? Use the --force, Luke",
-    
     "Why was the function sad after a successful first call? It didn’t get a callback.",
-
     "What did the spider do on the computer? Made a website!",
-
     "What did the computer do at lunchtime? Had a byte!",
 ]
 
 @app.get("/")
 def tell_a_joke():
     joke = random.choice(jokes)
-    return f"<p>{joke}</p>"
+    return flask.render_template('joke.html', joke_text=joke)
