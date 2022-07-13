ae8e7df49414be8ca268792f8bda05362bae1afb
Ray Belle <bellexdawn@gmail.com>
2022-07-13 16:13:09 -0700

    Cleaning up formatting

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ import random
 
 app = Flask(__name__)
 
+
 @app.get("/")
 def tell_a_joke():
     print("Beginning to tell a joke")
     joke = random.choice(data.jokes)
     print(f"The joke we are telling is {joke}")
-    return flask.render_template('joke.html', joke_text=joke)
+    return flask.render_template("joke.html", joke_text=joke)
