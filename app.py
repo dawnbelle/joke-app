7945b9fc96e2369b370102541709078cc9f97cd5
Ray Belle <bellexdawn@gmail.com>
2022-07-13 14:43:05 -0700

    Added the jokes.

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+jokes = [
+    "What can you do if you cannot push your git changes? Use the --force, Luke",
+    
+    "Why was the function sad after a successful first call? It didn’t get a callback.",
+
+    "What did the spider do on the computer? Made a website!",
+
+    "What did the computer do at lunchtime? Had a byte!",
+]
+
 @app.get("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def tell_a_joke():
+    joke = random.choice(jokes)
+    return f"<p>{joke}</p>"
